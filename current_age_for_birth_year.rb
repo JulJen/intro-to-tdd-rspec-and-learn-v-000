7eab6a9572bcbe8f679382bde2cdee7ec368777f
Julia J <juliajen.prog@gmail.com>
2017-09-24 19:36:49 +0000

    automatically backed up by learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -12,6 +12,10 @@
 # end
 
 
+# def current_age_for_birth_year(birth_year)
+#   2003 - birth_year
+# end
+
 def current_age_for_birth_year(birth_year)
-  2003 - birth_year
+  Time.now.year - birth_year
 end
